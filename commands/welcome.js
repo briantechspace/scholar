dda8a83548fabf93d68a6195ed1277a79b337e53
brayo-tech <oumabriansoja@gmail.com>
2026-01-13 09:49:14 -0900

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -1,7 +1,16 @@
-const groups = require('../database/groups.json')
+const fs = require('fs')
+const path = './database/groups.json'
+
+function getGroups() {
+  if (!fs.existsSync(path)) {
+    fs.writeFileSync(path, JSON.stringify({}))
+  }
+  return JSON.parse(fs.readFileSync(path))
+}
 
 async function handleWelcomeEvent(sock, data) {
-  if (!groups[data.id]?.welcome) return
+  const groups = getGroups()
+  if (!groups[data.id] || !groups[data.id].welcome) return
 
   for (const user of data.participants) {
     await sock.sendMessage(data.id, {
