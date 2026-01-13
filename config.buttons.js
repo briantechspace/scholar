1b4cf8122e733a3cfe76044015debce2db95d180
brayo-tech <oumabriansoja@gmail.com>
2026-01-13 09:10:13 -0900

    Create config.buttons.js

---
+++ config.buttons.js
@@ -0,0 +1,22 @@
+module.exports.menuButtons = [
+  {
+    buttonId: 'support',
+    buttonText: { displayText: '❤️ Support' },
+    type: 1
+  },
+  {
+    buttonId: 'tutorials',
+    buttonText: { displayText: '📺 Tutorials' },
+    type: 1
+  },
+  {
+    buttonId: 'channel',
+    buttonText: { displayText: '📢 Channel' },
+    type: 1
+  },
+  {
+    buttonId: 'contact',
+    buttonText: { displayText: '📞 Contact Brian' },
+    type: 1
+  }
+]
